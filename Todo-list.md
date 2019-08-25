a72f9317ad2cda7065a981c05249f2963ffcc7c5
Kyd <julien.coche@protonmail.com>
2019-08-25 16:18:34 +0200

    Add creds docstrings and overall project guidelines

---
+++ Todo-list.md
@@ -0,0 +1,40 @@
+# Miner des tweets
+
+1. Mode **Stream**
+   Args :
+   
+   - API
+   
+   - Un dir de destination
+   
+   Renvoit :
+   
+   - Un fichier de destination
+   
+   - Un état?
+     
+   
+   
+   
+
+1. Mode **Getter**
+
+Exemple utilisation: 
+
+```py
+api = AuthApi("getter", cred1, cred2, cred3, cred4)
+>>> miner1 = Miner("getter")
+# Les données sont récupérés depuis un fichier qui contient les ids.
+# Deux possibilités d'écriture: fichier ou BDD
+>>> miner1.from_file("../data/CrisisLexT26/Colorado_2016_ids.csv")
+.to("../data/tweets-from-ids/")
+# ou
+>>> miner1.from_file("../data/CrisisLexT26/Colorado_2016_ids.csv")
+.to("database").db_config(arg1, arg2, arg3...)
+# Puis on passe l'api qui sera utilisée pour récupérer les données.
+# Cela lance également la récupération des données jusqu'à ce qu'on atteigne la limite d'appels
+>>> miner1.mine(api)
+
+```
+
+
--- pumpy/creds.py
+++ pumpy/creds.py
@@ -6,60 +6,72 @@ from tweepy import API
 
 
 class AuthApi(object):
+    """
+    Return a Tweepy's API object. Used to perform calls to Twitter's API.
+    Return a different API object according to the chosen mode and the tokens provided.
+    Modes :
+    "getter" -> get tweets according to an ids list.
+    "stream" -> stream tweets.
+    
+    Arguments:
+        mode: {str} -- 'getter' or 'stream
+        token {str} -- Access token
+        token_secret {str} -- Access token secret
+        key {str} -- Consumer API key
+        key_secret {str} -- Consumer API key secret
+    
+    Raises:
+        ValueError: Invalid credentials
+        ValueError: The 'mode' argument should be 'stream' or 'getter'
+    
+    Returns:
+        [API] -- A Tweepy's API object
+    """
+
     def __init__(
         self,
         mode: str,
-        access_token: str,
-        access_token_secret: str,
-        api_key: str = "",
-        api_key_secret: str = "",
+        token: str,
+        token_secret: str,
+        key: str = "",
+        key_secret: str = "",
     ):
         self.mode = mode
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
-        self.api_key = api_key
-        self.api_key_secret = api_key_secret
+        self.token = token
+        self.token_secret = token_secret
+        self.key = key
+        self.key_secret = key_secret
 
     @property
     def generate_api(self) -> API:
-        if self.mode == "get":
+        if self.mode == "getter":
             if (
-                self.access_token
-                and self.access_token_secret
-                and (self.api_key == False or self.api_key_secret == False)
+                self.token
+                and self.token_secret
+                and (self.key == False or self.key_secret == False)
             ):
-                logger.debug("Get - AppAuth")
-                auth = AppAuthHandler(self.access_token, self.access_token_secret)
+                logger.debug("Getter - AppAuth")
+                auth = AppAuthHandler(self.token, self.token_secret)
 
-            elif (
-                self.access_token
-                and self.access_token_secret
-                and self.api_key
-                and self.api_key_secret
-            ):
-                logger.debug("Get - OAuth")
-                auth = OAuthHandler(
-                    self.access_token, self.access_token_secret
-                ).set_access_token(self.api_key, self.api_key_secret)
+            elif self.token and self.token_secret and self.key and self.key_secret:
+                logger.debug("Getter - OAuth")
+                auth = OAuthHandler(self.token, self.token_secret).set_token(
+                    self.key, self.key_secret
+                )
 
             else:
                 raise ValueError("Invalid credentials")
 
         elif self.mode == "stream":
-            if (
-                self.access_token
-                and self.access_token_secret
-                and self.api_key
-                and self.api_key_secret
-            ):
+            if self.token and self.token_secret and self.key and self.key_secret:
                 logger.debug("Stream - OAuth")
-                auth = OAuthHandler(
-                    self.access_token, self.access_token_secret
-                ).set_access_token(self.api_key, self.api_key_secret)
+                auth = OAuthHandler(self.token, self.token_secret).set_token(
+                    self.key, self.key_secret
+                )
 
             else:
                 raise ValueError("Invalid credentials")
         else:
-            raise ValueError("The 'mode' argument should be 'stream' or 'get'")
+            raise ValueError("The 'mode' argument should be 'stream' or 'getter'")
 
         return API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
