009a6465cc2d8ad925983a51bef0dfc2096ff6bd
Kyd <julien.coche@protonmail.com>
2019-08-26 17:47:59 +0200

    Wrote tests for from_file() method

--- tests/test_twitter_mining.py
+++ tests/test_twitter_mining.py
@@ -1,5 +1,37 @@
-import pumpy.twitter_mining
+from pumpy.twitter_mining import Miner
+import pytest
+from pytest_mock import mocker
+import path
 
 
-def test():
-    pass
+@pytest.fixture
+def minerg():
+    return Miner("getter")
+
+
+@pytest.fixture
+def miners():
+    return Miner("stream")
+
+
+def test_miner_mode(minerg, miners):
+    assert minerg.mode == "getter"
+    assert miners.mode == "stream"
+
+
+def test_miner_from_file_exist(minerg, miners):
+    with pytest.raises(FileNotFoundError):
+        miners.from_file("../data/CrisisLexT26/Colorado_2016_ids.csv", 1)
+
+
+def test_miner_from_file_getter_only(mocker, miners):
+    mocker.patch("path.Path.exists")
+    with pytest.raises(ValueError):
+        miners.from_file(path, 1)
+
+
+def test_miner_output(mocker, minerg):
+    mocker.patch("path.Path.exists")
+    result = minerg.from_file("this/is/a/path.txt", 1)
+    assert result.input_file == "this/is/a/path.txt"
+    assert result.index_ids == 1
