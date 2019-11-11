048a66ace1269900298dac525b2dba5f0704079f
Kyd <julien.coche@protonmail.com>
2019-11-11 18:11:02 -0500

    Add more fields in setup()

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="pumpy", packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pumpy",
+    version="0.0.1",
+    author="Kydlaw",
+    author_email="julien.coche@protonmail.com",
+    description="A Tweepy interface to collect tweets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kydlaw/pumpy",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["loguru", "path.py", "pymongo", "tweepy"],
+    python_requires=">=3.6",
+)
+
