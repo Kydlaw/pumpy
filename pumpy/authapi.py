4fddf37284efde75fce1099899e150d7e02b956e
Kyd <julien.coche@protonmail.com>
2019-11-11 18:12:31 -0500

    Rename creds.py in authapi.py. It provides more information about what it is actually doing.

---
+++ pumpy/authapi.py
@@ -0,0 +1,91 @@
+#  coding: utf-8
+
+from loguru import logger
+from tweepy import API, Stream
+from tweepy.auth import AppAuthHandler, OAuthHandler
+from tweepy.streaming import StreamListener
+
+
+class AuthApi(API):
+    """
+    Return a Tweepy's API object. Used to perform calls to Twitter's API.
+    Return a different API object according to the chosen mode and the tokens provided.
+    Modes :
+    "getter" -> get tweets according to an ids list.
+    "stream" -> stream tweets.
+    
+    Arguments:
+        mode: {str} -- 'getter' or 'stream'
+        access_token {str} -- Access access_token
+        access_token_secret {str} -- Access access_token secret
+        consumer_api_key {str} -- Consumer API consumer_api_key
+        consumer_api_secret {str} -- Consumer API consumer_api_key secret
+    
+    Raises:
+        ValueError: Invalid credentials
+        ValueError: The 'mode' argument should be 'stream' or 'getter'
+    
+    Returns:
+        [API] -- A Tweepy's API object
+    """
+
+    def __init__(
+        self,
+        mode: str,
+        access_token: str,
+        access_token_secret: str,
+        consumer_api_key: str = "",
+        consumer_api_secret: str = "",
+    ):
+        super().__init__()
+        self.mode = mode
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+        self.consumer_api_key = consumer_api_key
+        self.consumer_api_secret = consumer_api_secret
+
+    @property
+    def generate_api(self) -> API:
+        if self.mode == "getter":
+            if (
+                self.access_token
+                and self.access_token_secret
+                and (
+                    self.consumer_api_key == False or self.consumer_api_secret == False
+                )
+            ):
+                logger.debug("Getter - AppAuth")
+                auth = AppAuthHandler(self.access_token, self.access_token_secret)
+
+            elif (
+                self.access_token
+                and self.access_token_secret
+                and self.consumer_api_key
+                and self.consumer_api_secret
+            ):
+                logger.debug("Getter - OAuth")
+                auth = OAuthHandler(self.access_token, self.access_token_secret)
+                auth.set_access_token(self.consumer_api_key, self.consumer_api_secret)
+
+            else:
+                raise ValueError("Invalid arguments")
+
+            return API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+        elif self.mode == "stream":
+            if (
+                self.access_token
+                and self.access_token_secret
+                and self.consumer_api_key
+                and self.consumer_api_secret
+            ):
+                logger.debug("Stream - OAuth")
+                auth = OAuthHandler(self.access_token, self.access_token_secret)
+                auth.set_access_token(self.consumer_api_key, self.consumer_api_secret)
+
+            else:
+                raise ValueError("Invalid credentials")
+
+            return (auth, self.mode)
+        else:
+            raise ValueError("The 'mode' argument should be 'stream' or 'getter'")
