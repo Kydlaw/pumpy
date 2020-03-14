bfa2cf44aeaf6b4f0d92e1f7790119ec4c9d75d1
Kydlawj <kydlawj@pm.me>
2020-03-14 02:03:57 -0400

    Add instructions

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# pumpy
+# Pumpy
 A Twitter pump, for research purpose. Build on Tweepy shoulders.
 
 This repo provides tools to:
 
-- Get past data from tweet ids
-- Get data through the Twitter's streaming interface
-- Convert tweets in an invalid JSON format, to valid ones.
+- Get past data from tweet ids.
+- Get data through the Twitter's streaming interface.
+- Convert tweets in an invalid JSON format into a valid one.
+
+# Instructions
+
+- Pumpy requires MongoDB for database storage (see [MongoDB install](https://docs.mongodb.com/manual/administration/install-community)
+to install).
+- `git clone https://github.com/Kydlaw/pumpy.git`
+- Install dependancies from requirements.txt (`pip install -r requirements.txt`). Use of a python virtual
+environment is prefered.
+- Modify the wanted parameters in `main.py`
+- Launch the script with `python main.py`
+- ... should works, somehow, maybe? who knows?
---
+++ main.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import os
+
+from pumpy.twitter_mining import MinerStream
+from pumpy.authapi import AuthApi
+
+############################ THINGS TO CHANGE ############################
+# - Set your Twitter's access tokens/keys as environment variables (in your .zshrc or
+# activate file of your python virtual environment)
+# OR remove the "os.environ.get("...") and copy/paste your tokens/keys as strings
+# - (Optional) You can change the db's name and collection's name in the .db_config
+# (mine are "tweets" and "covid" for example)
+# - The keywords are defined in .search(...). The format is commat separated strings.
+
+
+def main():
+    miner = MinerStream()
+    miner.auth_keys.append(
+        AuthApi(
+            access_token=os.environ.get("ACCESS_TOKEN"),
+            access_token_secret=os.environ.get("ACCESS_SECRET"),
+            consumer_api_key=os.environ.get("CONSUMER_KEY"),
+            consumer_api_secret=os.environ.get("CONSUMER_SECRET"),
+        )
+    )
+
+    miner.to("database")
+    miner.db_config(db="tweets", collection="covid")
+    miner.search("covid", "corona", "YOLO", "Trump")
+    miner.mine()
+
+
+if __name__ == "__main__":
+    main()
